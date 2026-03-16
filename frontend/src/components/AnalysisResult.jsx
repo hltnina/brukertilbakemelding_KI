8d0e21c358f0abe1d2e7d46c2b56702d7ee59bae
Christofferns <144909120+Christofferns@users.noreply.github.com>
2026-03-16 14:06:34 +0100

    Lagt til "Issue labels"

--- frontend/src/components/AnalysisResult.jsx
+++ frontend/src/components/AnalysisResult.jsx
@@ -7,6 +7,14 @@ function AnalysisResult() {
         <label>Tittel på sak</label>
         <div className="analysis-value-box">Tittel</div>
       </div>
+
+      <div className="analysis-field">
+        <label>Issue Labels</label>
+        <div className="analysis-tags">
+          <div className="analysis-tag">Label</div>
+          <div className="analysis-tag">WCAG-nivå</div>
+        </div>
+      </div>
     </div>
   )
 }
