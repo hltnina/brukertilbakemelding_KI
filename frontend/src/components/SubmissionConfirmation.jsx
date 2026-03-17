8d639df2c6bd89e4647f8046caffbacf416dc431
Christofferns <144909120+Christofferns@users.noreply.github.com>
2026-03-17 12:53:30 +0100

    Laget "bekreftelse på innsending" og blå boks

---
+++ frontend/src/components/SubmissionConfirmation.jsx
@@ -0,0 +1,9 @@
+function SubmissionConfirmation() {
+  return (
+    <div className="confirmation-shell">
+      <h3>Bekreftelse på innsending</h3>
+    </div>
+  )
+}
+
+export default SubmissionConfirmation
