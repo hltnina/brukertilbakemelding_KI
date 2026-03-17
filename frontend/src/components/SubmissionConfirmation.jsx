6c8ff0d0cd836bddc614cbeea7dfc9979ecce2f2
Christofferns <144909120+Christofferns@users.noreply.github.com>
2026-03-17 12:56:43 +0100

    Lagt til tekstboksen

--- frontend/src/components/SubmissionConfirmation.jsx
+++ frontend/src/components/SubmissionConfirmation.jsx
@@ -2,6 +2,19 @@ function SubmissionConfirmation() {
   return (
     <div className="confirmation-shell">
       <h3>Bekreftelse på innsending</h3>
+
+      <div className="confirmation-message-box">
+        <p>
+          Din innsending er nå sendt inn til Github Issues, og har{' '}
+          <strong>saksnummeret #1.</strong>
+        </p>
+
+        <p>
+          Se saken i Github Issues ved å trykke på{' '}
+          <strong>“Se Github Issue #1”</strong>, eller generer en analyse av en
+          ny sak ved å trykke på <strong>“Ny sak”</strong>.
+        </p>
+      </div>
     </div>
   )
 }
